16f8ebca4bd2d0d72a838bdbbd6375baa1f1b0eb
faezebg <bagherianfaeze4@gmail.com>
2023-02-01 12:51:00 +0330

    added moves

--- faz00.c
+++ faz00.c
@@ -3,13 +3,21 @@
 #include<stdlib.h>
 #include<time.h>
 
+int pix[90];
+int piy[90];
+
 int xfirst1 = 194, yfisrt1 = 490;
 int xfirst2 = 194, yfisrt2 = 490;
 int xsecond1 = 572, ysecond1 = 114;
 int xsecond2 = 572, ysecond2 = 114;
 
+int first1 = 1, first2 = 1;
+int second1 = 81, second2 = 81;
+
 int x_tas = 371;
 int y_tas = 29;
+
+int TAS = 0, turn = -1;
 
 int tas() {
     int arr[6], T;
@@ -27,6 +35,78 @@ int tas() {
     return arr[T % 6];
 }
 
+void set_pos() {
+    int C = 47;
+    pix[1] = 194;
+    piy[1] = 490;
+    //111111111111111111111111111111111111111111111111111
+    for (int i = 2; i < 10; i++) {
+        pix[i] = pix[i - 1] + C;
+        piy[i] = piy[i - 1];
+    }
+    //2222222222222222222222222222222222222222222222222222
+    for (int i = 1; i < 10; i++) {
+        pix[19 - i] = pix[i];
+        piy[19 - i] = piy[i] - C;
+    }
+    //3333333333333333333333333333333333333333333333333333
+    for (int i = 1; i < 10; i++) {
+        pix[18 + i] = pix[19 - i];
+        piy[18 + i] = piy[19 - i] - C;
+    }
+    //4444444444444444444444444444444444444444444444444444
+    for (int i = 1; i < 10; i++) {
+        pix[37 - i] = pix[18 + i];
+        piy[37 - i] = piy[18 + i] - C;
+    }
+    //5555555555555555555555555555555555555555555555555555
+    for (int i = 1; i < 10; i++) {
+        pix[36 + i] = pix[37 - i];
+        piy[36 + i] = piy[37 - i] - C;
+    }
+    //6666666666666666666666666666666666666666666666666666
+    for (int i = 1; i < 10; i++) {
+        pix[55 - i] = pix[36 + i];
+        piy[55 - i] = piy[36 + i] - C;
+    }
+    //7777777777777777777777777777777777777777777777777777
+    for (int i = 1; i < 10; i++) {
+        pix[54 + i] = pix[55 - i];
+        piy[54 + i] = piy[55 - i]  - C;
+    }
+    //8888888888888888888888888888888888888888888888888888
+    for (int i = 1; i < 10; i++) {
+        pix[73 - i] = pix[54 + i];
+        piy[73 - i] = piy[54 + i] - C;
+    }
+    //9999999999999999999999999999999999999999999999999999
+    for (int i = 1; i < 10; i++) {
+        pix[72 + i] = pix[73 - i];
+        piy[72 + i] = piy[73 - i] - C;
+    }
+}
+
+int check(int x, int sw) {
+    int cnt = 0;
+    if (sw == 1) {
+        if (first1 + x <= 81 && first1 + x >= 1) {
+            cnt = 10;
+        }
+        if (first2 + x <= 81 && first2 + x >= 1) {
+            cnt++;
+        }
+    }
+    else {
+        if (second1 + x <= 81 && second1 + x >= 1) {
+            cnt = 10;
+        }
+        if (second2 + x <= 81 && second2 + x >= 1) {
+            cnt++;
+        }
+    }
+    return cnt;
+}
+
 int main() {
     const int windowwidth = 800;
     const int windowheight = 600;
@@ -36,23 +116,22 @@ int main() {
     
     Texture2D board = LoadTexture("resources/final_board.png"); 
 
-    Texture2D first1 = LoadTexture("resources/rsz_sefid.png"); 
-    Texture2D first2 = LoadTexture("resources/rsz_sefid.png"); 
-    Texture2D second1 = LoadTexture("resources/rsz_siah.png"); 
-    Texture2D second2 = LoadTexture("resources/rsz_siah.png"); 
-
-    int TAS = 0, turn = 1;
+    Texture2D sefid1 = LoadTexture("resources/sefid1.png"); 
+    Texture2D sefid2 = LoadTexture("resources/sefid2.png"); 
+    Texture2D siah1 = LoadTexture("resources/siah1.png"); 
+    Texture2D siah2 = LoadTexture("resources/siah2.png"); 
+    set_pos();
 
     while(!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(RAYWHITE); //color(245, 245, 245, 255)
-        DrawTexture(board, windowwidth/2 - board.width/2, windowheight/2 - board.height/2, WHITE);
         
-        DrawTexture(first1, xfirst1, yfisrt1, WHITE);
-        DrawTexture(first2, xfirst2, yfisrt2, WHITE);
-        DrawTexture(second1, xsecond1, ysecond1, WHITE);
-        DrawTexture(second2, xsecond2, ysecond2, WHITE);
-
+        // premium
+        if (GetMouseX() < 449 && GetMouseX() > 355 &&GetMouseY() < 580 && GetMouseY() > 550 && IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) {
+            sefid1 = LoadTexture("resources/sefidp.png"); 
+            sefid2 = LoadTexture("resources/sefidp.png"); 
+            siah1 = LoadTexture("resources/siahp.png"); 
+            siah2 = LoadTexture("resources/siahp.png");
+        }
+        // click on tas
         if (GetMouseX() < 431 && GetMouseX() > 368 &&GetMouseY() < 90 && GetMouseY() > 27 && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
             if (tas() == -3) {
                 TAS = tas();
@@ -74,20 +153,75 @@ int main() {
             }
             turn *= -1;
         }
+
+        //kodom mohre
         if (turn == 1) {
+            if (IsKeyPressed(KEY_A) || check(TAS, turn) == 10) {
+                first1 += TAS;
+                xfirst1 = pix[first1];
+                yfisrt1 = piy[first1];
+            }
+            else if(IsKeyPressed(KEY_B) || check(TAS, turn) == 1) {
+                first2 += TAS;
+                xfirst2 = pix[first2];
+                yfisrt2 = piy[first2];
+            }
+        }
+        else {
+            if (IsKeyPressed(KEY_A) || check(TAS, turn) == 10) {
+                second1 += TAS;
+                xsecond1 = pix[second1];
+                ysecond1 = piy[second1];
+            }
+            else if(IsKeyPressed(KEY_B) || check(TAS, turn) == 1) {
+                second2 += TAS;
+                xsecond2 = pix[second2];
+                ysecond2 = piy[second2];
+            }
+        }
+        /////////////////////////////////////////////////////////////
+        FILE *inputfile;
+        FILE *outputfile;
+        inputfile = fopen("input.txt", "rt");
+        outputfile = fopen("output.txt", "wt");
+        fprintf("outputfile", "%d %d %d %d %d", turn, first1, first2, second1, second2);
+        fclosall();
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        BeginDrawing();
+        ClearBackground(RAYWHITE); //color(245, 245, 245, 255)
+        DrawTexture(board, windowwidth/2 - board.width/2, windowheight/2 - board.height/2, WHITE);
+        
+        //mohre ha
+        DrawTexture(sefid1, xfirst1, yfisrt1, WHITE);
+        DrawTexture(sefid2, xfirst2, yfisrt2, WHITE);
+        DrawTexture(siah1, xsecond1, ysecond1, WHITE);
+        DrawTexture(siah2, xsecond2, ysecond2, WHITE);
+
+        //nobat
+        if (turn == -1) {
             DrawText("WHITE :", 81, 48, 30, MAROON);
         }
         else {
             DrawText("BLACK :", 81, 48, 30, MAROON);
         }
         DrawText(TextFormat("%i", TAS), 387, 37, 40, BLACK);
+
+        //bepors kodom mohre
+        if (check(TAS, turn) > 10 && TAS) {
+            DrawRectangle(532, 25, 200, 60, GREEN);
+            DrawText("which on?", 540, 27, 20, BLACK);
+            DrawText("press A or B", 540, 48, 20, BLACK);
+        }
+
+        // premium
+        DrawText("PREMIUM", 375, 567, 10, GRAY);
         EndDrawing();
     }
     UnloadTexture(board);
-    UnloadTexture(first1);
-    UnloadTexture(first2);
-    UnloadTexture(second1);
-    UnloadTexture(second2);
+    UnloadTexture(sefid1);
+    UnloadTexture(sefid2);
+    UnloadTexture(siah1);
+    UnloadTexture(siah2);
     CloseWindow();
     return 0; 
 }
